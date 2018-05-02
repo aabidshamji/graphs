9d37f8da6f9757129ad4e18bebc1c01f0640a1a0
Aabid Shamji <aabidshamji@gmail.com>
2018-05-02 15:41:24 -0500

    added comments

--- src/cities/solution.java
+++ src/cities/solution.java
@@ -4,7 +4,16 @@ import java.util.*;
 
 
 public class solution {
-
+	/*
+	 * @param n, the number of nodes in the graph 
+	 * @param m, the number of roads
+	 * @param c_lib, the cost of building a library 
+	 * @param c_road, the cost of building a road 
+	 * @param discovered, a boolean array that is true when the nodes has been discovered 
+	 * @param adjList, the graph represented as an adj list 
+	 * 
+	 * @returns, cost, a long that represents the minimum cost of giving each city access to a library 
+	 */
     public static long RoadsAndLibraries (int n, int m, long c_lib, long c_road, boolean[] discovered, ArrayList<ArrayList<Integer>> adjList) {
         long cost = 0; 
         for (int i = 0; i < adjList.size(); i++) {
@@ -22,6 +31,15 @@ public class solution {
         return cost; 
     }
     
+    /*
+     * Performs depth first search from the given node to return the number of road that are needed to be built 
+     * @param v, the node to start dfs from 
+     * @param count, the value to return 
+     * @param discovered, a boolean array that is true when the nodes has been discovered 
+     * @param adjList, adjList, the graph represented as an adj list
+     * 
+     * @return count, the number of roads that are in that sub-graph 
+     */
     public static long dfs (int v, long count, boolean[] discovered, ArrayList<ArrayList<Integer>> adjList) {
     	discovered[v] = true; 
         ArrayList<Integer> arr = adjList.get(v);
